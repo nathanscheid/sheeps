5ba96d620f62ee6d361aee1b905733588fe30021
e0x19 <esaupe@montessorihighschool.org>
2015-05-26 10:05:10 -0400

    menufunc
    Builtmenufunc

--- sheeps/ViewController.swift
+++ sheeps/ViewController.swift
@@ -7,16 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
+import Foundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        var screen: CGRect! = UIScreen.mainScreen().bounds
+        var width = screen.width
+        var height = screen.height
+
         func buildMenu()
         {
             self.view.backgroundColor = UIColor.blackColor()
+            var startbtn = UIButton.buttonWithType(UIButtonType.System) as UIButton
+            startbtn.frame = CGRectMake(width/2 - 25, height/2 - 30, 50, 25)
+            startbtn.backgroundColor = UIColor.greenColor()
+            startbtn.setTitle("Start", forState:UIControlState.Normal)
+            startbtn.addTarget(self, action:"startGame:", forControlEvents:UIControlEvents.TouchUpInside)
         }
+        
+        func startGame(sender: UIButton!)
+        {}
         
     }
 
