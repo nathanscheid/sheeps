43324bef8940b844a234f2b0929154cc2f4c1c7d
Razinmore <bsimon@c1mkrzg3dty3.montessorihighschool.org>
2015-05-26 15:11:13 -0400

    collision

--- sheeps/movement.swift
+++ sheeps/movement.swift
@@ -5,5 +5,25 @@
 //  Created by Ellis Saupe on 5/26/15.
 //  Copyright (c) 2015 Xterm2015. All rights reserved.
 //
-
 import Foundation
+import UIKit
+
+class rBox
+{
+    var width: Float = 0.0
+    var height: Float = 0.0
+    var x: Float = 0.0
+    var y: Float = 0.0
+    
+}
+
+
+func cTest(box1: rBox, box2: rBox)
+{
+    if(box1.x < box2.x + box2.width && box1.x + box1.width > box2.x && box1.y < box2.y + box2.height && box1.y + box1.height > box2.y)
+    {
+        
+        
+        
+    }
+}
