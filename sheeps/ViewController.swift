c20bec8b2daf098097ee0f376c221cad3a30c3df
e0x19 <esaupe@montessorihighschool.org>
2015-05-26 10:38:08 -0400

    addedstuff
    stuff

--- sheeps/ViewController.swift
+++ sheeps/ViewController.swift
@@ -19,19 +19,39 @@ class ViewController: UIViewController {
         var screen: CGRect! = UIScreen.mainScreen().bounds
         var width = screen.width
         var height = screen.height
-
         func buildMenu()
         {
             self.view.backgroundColor = UIColor.blackColor()
             var startbtn = UIButton.buttonWithType(UIButtonType.System) as UIButton
             startbtn.frame = CGRectMake(width/2 - 25, height/2 - 30, 50, 25)
-            startbtn.backgroundColor = UIColor.greenColor()
+            startbtn.backgroundColor = UIColor.redColor()
             startbtn.setTitle("Start", forState:UIControlState.Normal)
             startbtn.addTarget(self, action:"startGame:", forControlEvents:UIControlEvents.TouchUpInside)
+            self.view.addSubview(startbtn)
+            
+            var creditbtn = UIButton.buttonWithType(UIButtonType.System) as UIButton
+            creditbtn.frame = CGRectMake(width/2 - 25, height/2 + 5, 50, 25)
+            creditbtn.backgroundColor = UIColor.greenColor()
+            creditbtn.setTitle("Credits", forState:UIControlState.Normal)
+            creditbtn.addTarget(self, action:"showCredits:", forControlEvents:UIControlEvents.TouchUpInside)
+            self.view.addSubview(creditbtn)
         }
         
         func startGame(sender: UIButton!)
         {}
+        
+        func showCredits(sender: UIButton!)
+        {}
+        
+        func clearView()
+        {
+            for view in self.view.subviews
+            {
+                view.removeFromSuperview()
+            }
+        }
+        
+        buildMenu()
         
     }
 
