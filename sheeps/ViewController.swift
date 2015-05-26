feced0d678dd15447a29384aba5dbaa7e5fdd142
nathanscheid <nathanscheidler@Nathans-MacBook-Pro.local>
2015-05-26 09:17:41 -0400

    Initial Commit

---
+++ sheeps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sheeps
+//
+//  Created by Nathan Scheidler on 5/26/15.
+//  Copyright (c) 2015 Xterm2015. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
